4d21d0806b6dd9248140111fe59567ba3b566256
sudotx <64108848+sudotx@users.noreply.github.com>
2023-12-23 01:40:22 +0100

    chore: only solady imports, breaking changes to createBid functionality

--- src/MiyaTees.sol
+++ src/MiyaTees.sol
@@ -1,16 +1,16 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import {SafeTransferLib} from "@solmate/utils/SafeTransferLib.sol";
-import {ReentrancyGuard} from "@solmate/utils/ReentrancyGuard.sol";
-import {ERC721TokenReceiver, ERC721} from "@solmate/tokens/ERC721.sol";
+import {Ownable} from "@solady/auth/Ownable.sol";
+import {Receiver} from "@solady/accounts/Receiver.sol";
+import {SafeCastLib} from "@solady/utils/SafeCastLib.sol";
+import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
 
-contract MiyaTees is ReentrancyGuard, ERC721TokenReceiver {
+contract MiyaTeesAuction is Receiver, Ownable {
     /*//////////////////////////////////////////////////////////////
                                 ERRORS
     //////////////////////////////////////////////////////////////*/
     error InsufficientETHSent();
-    error WithdrawFailed();
     error AuctionNotOver();
     error AuctionEnded();
     error AuctionNotStartedYet();
@@ -22,12 +22,38 @@ contract MiyaTees is ReentrancyGuard, ERC721TokenReceiver {
     //////////////////////////////////////////////////////////////*/
     event BidPlaced(address indexed sender, uint256 amount);
     event AuctionStarted(uint256 endTime);
-    event Withdraw(address indexed bidder, uint256 amount);
     event End(address winner, uint256 amount);
+    event AuctionSettled(uint256 indexed nftId, address winner, uint256 amount);
+    event AuctionDurationUpdated(uint256 amount);
+    event AuctionBidIncrementUpdated(uint256 amount);
+
+    event AuctionTimeBufferUpdated(uint256 timeBuffer);
+
+    event AuctionReservePriceUpdated(uint256 reservePrice);
+
+    event AuctionReservePercentageUpdated(uint256 reservePercentage);
 
     /*//////////////////////////////////////////////////////////////
                             PRICING PARAMS
     //////////////////////////////////////////////////////////////*/
+
+    struct AuctionData {
+        address bidder;
+        uint96 amount;
+        uint40 startTime;
+        uint40 endTime;
+        uint96 withdrawable;
+        bool settled;
+        address miyaTees;
+        uint8 reservePercentage;
+        uint96 reservePrice;
+        uint96 bidIncrement;
+        uint32 duration;
+        uint32 timeBuffer;
+    }
+
+    AuctionData internal _auctionData;
+
     uint256 public endAt;
     bool public started;
     bool public ended;
@@ -35,104 +61,227 @@ contract MiyaTees is ReentrancyGuard, ERC721TokenReceiver {
     uint256 public highestBid;
     uint256 public immutable nftId;
     address payable public immutable seller;
-    ERC721 public immutable nft;
+    IERC721 public immutable nft;
     mapping(address => uint256) public refunds;
 
-    uint256 public constant AUCTION_DURATION = 3 days;
-    uint256 public constant BID_INCREMENT = 0.05 ether;
+    uint32 public constant AUCTION_DURATION = 3 days;
+    uint96 public constant BID_INCREMENT = 0.05 ether;
 
-    constructor(address payable _beneficiary, address _nft, uint256 _nftId) {
+    constructor(
+        address payable _beneficiary,
+        address _miyaTees,
+        uint256 _nftId,
+        uint96 reservePrice,
+        uint32 timeBuffer,
+        uint8 reservePercentage
+    ) payable {
         if (_beneficiary == address(0)) {
             revert ZeroAddress();
         }
-        if (_nft == address(0)) {
+        if (_miyaTees == address(0)) {
             revert ZeroAddress();
         }
-        nft = ERC721(_nft);
+
+        _auctionData.duration = AUCTION_DURATION;
+        _auctionData.bidIncrement = BID_INCREMENT;
+        _auctionData.miyaTees = _miyaTees;
+        _auctionData.timeBuffer = timeBuffer;
+        _auctionData.reservePrice = reservePrice;
+        _auctionData.reservePercentage = reservePercentage;
+
+        nft = IERC721(_miyaTees);
         nftId = _nftId;
         seller = payable(_beneficiary);
-    }
-
-    function startAuction() external {
-        if (started) {
-            revert AuctionAlreadyStarted();
-        }
-
-        started = true;
-        endAt = block.timestamp + AUCTION_DURATION;
 
         nft.transferFrom(msg.sender, address(this), nftId);
-
-        emit AuctionStarted(endAt);
     }
 
     /*//////////////////////////////////////////////////////////////
-                            STATE CHANGOORS
+                    PUBLIC/EXTERNAL VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    function bidTees() public payable {
-        if (!started) {
-            revert AuctionNotStartedYet();
-        }
-        if (ended) {
-            revert AuctionEnded();
-        }
 
-        if (block.timestamp > endAt) {
-            revert AuctionEnded();
-        }
-
-        if (msg.value < highestBid + BID_INCREMENT) {
-            revert InsufficientETHSent();
-        }
-
-        if (highestBid != 0) {
-            refunds[msg.sender] += msg.value;
-        }
-
-        highestBidder = msg.sender;
-        highestBid = msg.value;
-
-        emit BidPlaced(msg.sender, msg.value);
+    function hasEnded() public view returns (bool) {
+        // fix this
+        return block.timestamp >= AUCTION_DURATION;
     }
 
-    /**
-     * @dev Users can claim a refund
-     */
-    function withdrawPendingReturns() external nonReentrant {
-        uint256 bal = refunds[msg.sender];
-        refunds[msg.sender] = 0;
-        emit Withdraw(msg.sender, bal);
-        SafeTransferLib.safeTransferETH(msg.sender, bal); 
+    function auctionData() public view returns (AuctionData memory data) {
+        data = _auctionData;
     }
 
-    /**
-     * @dev End the auction
-     */
-    function endAuction() external nonReentrant {
-        if (block.timestamp > endAt) {
-            revert AuctionNotOver();
-        }
-        if (!started) {
-            revert AuctionNotStartedYet();
+    /*//////////////////////////////////////////////////////////////
+                    PUBLIC/EXTERNAL WRITE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function bidTees() external payable {
+        require(gasleft() > 150000);
+
+        // auto auction creation and settlement
+
+        bool creationFailed;
+        if (_auctionData.startTime == 0) {
+            // if auction not created
+            // create a new auction
+            creationFailed = !_createAuction();
+        } else if (hasEnded()) {
+            // if auction has ended, and is settled. try creating a new one
+            if (_auctionData.settled) {
+                creationFailed = !_createAuction();
+            } else {
+                // if auction has ended, but not settled. settle it
+                _settleAuction();
+                // try creating new one, after settling previous
+                if (!_createAuction()) {
+                    // if creation failed refund all ETH sent
+                    SafeTransferLib.forceSafeTransferETH(msg.sender, msg.value);
+                    return;
+                }
+            }
         }
 
-        if (ended) {
-            revert AuctionEnded();
-        }
+        require(!creationFailed, "cannot create auction");
 
-        ended = true;
-        if (highestBidder != address(0)) {
-            nft.safeTransferFrom(address(this), highestBidder, nftId);
-            SafeTransferLib.safeTransferETH(seller, address(this).balance);
+        // bidding logic
+
+        address lastBidder = _auctionData.bidder;
+        uint256 amount = _auctionData.amount;
+        uint256 endTime = _auctionData.endTime;
+
+        if (amount == 0) {
+            require(msg.value > _auctionData.reservePrice, "bid below reserve price");
         } else {
-            nft.safeTransferFrom(address(this), seller, nftId);
+            require(msg.value > amount + _auctionData.bidIncrement);
         }
 
-        emit End(highestBidder, highestBid);
+        _auctionData.bidder = msg.sender;
+        _auctionData.amount = SafeCastLib.toUint96(msg.value);
+
+        if (_auctionData.timeBuffer == 0) {
+            // emit auction bid
+            // emit AuctionBid(bonklerId, msg.sender, msg.value, false);
+            emit BidPlaced(msg.sender, msg.value);
+        } else {
+            // Extend the auction if the bid was received within `timeBuffer` of the auction end time.
+            uint256 extendedTime = block.timestamp + _auctionData.timeBuffer;
+            // Whether the current timestamp falls within the time extension buffer period.
+            bool extended = endTime < extendedTime;
+            // emit AuctionBid(bonklerId, msg.sender, msg.value, extended);
+            emit BidPlaced(msg.sender, msg.value);
+
+            if (extended) {
+                _auctionData.endTime = SafeCastLib.toUint40(extendedTime);
+                // emit AuctionExtended(bonklerId, extendedTime);
+            }
+        }
+
+        if (amount != 0) {
+            // refund last bidder
+            SafeTransferLib.forceSafeTransferETH(lastBidder, amount);
+        }
     }
 
-    // allows the contract receive 721 tokens
-    function onERC721Received(address, address, uint256, bytes calldata) public virtual override returns (bytes4) {
-        return ERC721TokenReceiver.onERC721Received.selector;
+    function settleAuction() external {
+        require(block.timestamp > endAt);
+        require(started);
+        require(!ended);
+        // require(block.timestamp >= _auctionData.endTime);
+        require(_auctionData.startTime != 0);
+        require(_auctionData.bidder != address(0));
+        require(!_auctionData.settled);
+        _settleAuction();
     }
+
+    /*//////////////////////////////////////////////////////////////
+                        ADMIN WRITE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function withdrawETH() external onlyOwner {
+        uint256 amount = _auctionData.withdrawable;
+        _auctionData.withdrawable = 0;
+        SafeTransferLib.forceSafeTransferETH(msg.sender, amount);
+    }
+
+    function setReservePrice(uint8 reservePercentage) external onlyOwner {
+        _checkReservePercentage(reservePercentage);
+        _auctionData.reservePercentage = reservePercentage;
+        emit AuctionReservePriceUpdated(reservePercentage);
+    }
+
+    function setBidIncrement(uint96 bidIncrement) external onlyOwner {
+        _checkBidIncrement(bidIncrement);
+        _auctionData.bidIncrement = bidIncrement;
+        emit AuctionBidIncrementUpdated(bidIncrement);
+    }
+
+    function setDuration(uint32 duration) external onlyOwner {
+        _checkDuration(duration);
+        _auctionData.duration = duration;
+        emit AuctionDurationUpdated(duration);
+    }
+
+    function setTimeBuffer(uint32 timeBuffer) external onlyOwner {
+        _auctionData.timeBuffer = timeBuffer;
+        emit AuctionTimeBufferUpdated(timeBuffer);
+    }
+
+    function setReservePercentage(uint8 reservePercentage) external onlyOwner {
+        _checkReservePercentage(reservePercentage);
+        _auctionData.reservePercentage = reservePercentage;
+        emit AuctionReservePercentageUpdated(reservePercentage);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        INTERNAL/PRIVATE HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    function _createAuction() internal returns (bool) {
+        uint256 endT = block.timestamp + AUCTION_DURATION;
+
+        _auctionData.bidder = address(1);
+        _auctionData.amount = 0;
+        _auctionData.startTime = SafeCastLib.toUint40(block.timestamp);
+        _auctionData.endTime = SafeCastLib.toUint40(endT);
+        _auctionData.settled = false;
+
+        emit AuctionStarted(endT);
+
+        return true;
+    }
+
+    function _settleAuction() internal {
+        address bidder = _auctionData.bidder;
+        uint256 amount = _auctionData.amount;
+        uint256 withdrawable = _auctionData.withdrawable;
+        uint256 reservePercentage = _auctionData.reservePercentage;
+        address miyaTees = _auctionData.miyaTees;
+
+        uint256 miyaShares = amount * reservePercentage / 100;
+        withdrawable += amount - miyaShares;
+
+        IERC721(miyaTees).transferFrom(address(this), bidder, nftId);
+
+        _auctionData.settled = true;
+        _auctionData.withdrawable = SafeCastLib.toUint96(withdrawable);
+
+        emit AuctionSettled(nftId, bidder, amount);
+    }
+
+    function _checkReservePercentage(uint8 reservePercentage) internal pure {
+        require(reservePercentage < 101, "reserve % exceeds 100");
+    }
+
+    function _checkReservePrice(uint96 reservePrice) internal pure {
+        require(reservePrice != 0, "reserve price must be greater than 0");
+    }
+
+    function _checkBidIncrement(uint96 bidIncrement) internal pure {
+        require(bidIncrement != 0, "bid increment must be greater than 0");
+    }
+
+    function _checkDuration(uint32 duration) internal pure {
+        require(duration != 0, "duration must be greater than 0");
+    }
+}
+
+interface IERC721 {
+    function safeTransferFrom(address from, address to, uint256 id) external;
+    function transferFrom(address from, address to, uint256 id) external;
 }
