68b2604d7c68b0254e9483396737f5ce0b469b59
sudotx <64108848+sudotx@users.noreply.github.com>
2024-01-16 22:17:20 +0100

    🎁

---
+++ test/unit/concrete/bidTees/bidTees.t.sol
@@ -0,0 +1,25 @@
+import {MiyaTeesTest} from "../../../utils/MiyaTees.t.sol";
+
+pragma solidity 0.8.0;
+
+contract bidTeestsol is MiyaTeesTest {
+    modifier whenNoAuctionExists() {
+        _;
+    }
+
+    function test_RevertGiven_MsgsenderIsAContract() external whenNoAuctionExists {
+        // it should revert
+    }
+
+    function test_GivenMsgsenderIsAnEOA() external whenNoAuctionExists {
+        // it should create a new auction, with default values
+    }
+
+    modifier givenAnAuctionExists() {
+        _;
+    }
+
+    function test_GivenCurrentMsgsenderBidsHigher() external givenAnAuctionExists {
+        // it should refund the previous msgsender a bit less than was sent in
+    }
+}
